ef658b952a7d87dca5646682980e8421d30045e5
Ahmed Raouf <web@pbc-egy.com>
2019-11-21 10:19:32 +0200

    'ad'

--- resources/views/website/client.blade.php
+++ resources/views/website/client.blade.php
@@ -21,7 +21,7 @@
         <div class="container">
             <div class="row mt-50">
                  @foreach($clients as $client)
-                <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                <div class="col-md-3 col-sm-4 col-xs-12 wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
                     <div class="white-bg text-center display-table mb-30 full-width client-logo-height">
                         <div class="">
                             <a href="#"><img src="{{url($client->image->path)}}" alt="01"></a>
